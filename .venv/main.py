db2e9bfcb2a48228c99783873a403f6b6ba94238
Ravali-goud <167891512+Ravali-goud@users.noreply.github.com>
2024-12-30 23:43:25 +0530

    Render the titles

--- .venv/main.py
+++ .venv/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1,col2=st.columns(2)
@@ -19,3 +20,17 @@ content2="""
 Below you can find some of the apps I have built in Python.Feel free to contact me!
 """
 st.write(content2)
+
+col3,col4=st.columns(2)
+
+df=pandas.read_csv("pythonProject/.venv/data.csv")
+
+
+with col3:
+    for index,row in df[1:10].iterrows():
+        st.header(row["Column1"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["Column1"])
+
