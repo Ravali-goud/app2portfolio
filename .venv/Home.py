34fa3188dbc408e47b42e53f5870d7abea434458
Ravali-goud <167891512+Ravali-goud@users.noreply.github.com>
2024-12-31 22:59:08 +0530

    commit1

--- .venv/Home.py
+++ .venv/Home.py
@@ -5,7 +5,8 @@ st.set_page_config(layout="wide")
 col1,col2=st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg",width=600)
+    st.image("./images/photo.jpg", width=600)
+
 with col2:
     st.title("Ravali Goud")
     content="""
