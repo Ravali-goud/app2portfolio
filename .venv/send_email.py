695c8bdb62395d6efb4d41aa887ae96b0f550a9c
Ravali-goud <167891512+Ravali-goud@users.noreply.github.com>
2024-12-31 22:15:28 +0530

    send email using the form

--- .venv/send_email.py
+++ .venv/send_email.py
@@ -1,19 +1,15 @@
 import smtplib,ssl
 
-host="smtp.gmail.com"
-port=465
+def send_email(message):
+    host="smtp.gmail.com"
+    port=465
 
-username="gravali919@gmail.com"
-password="eijmzdiyhvofrrer"
+    username="gravali919@gmail.com"
+    password="eijmzdiyhvofrrer"
 
-receiver="gravali919@gmail.com"
-context=ssl.create_default_context()
+    receiver="gravali919@gmail.com"
+    context=ssl.create_default_context()
 
-message="""\
-Subject:Hi!
-How are you
-Bye!
-"""
-with smtplib.SMTP_SSL(host,port,context=context) as server:
-    server.login(username,password)
-    server.sendmail(username,receiver,message)
+    with smtplib.SMTP_SSL(host,port,context=context) as server:
+        server.login(username,password)
+        server.sendmail(username,receiver,message)
