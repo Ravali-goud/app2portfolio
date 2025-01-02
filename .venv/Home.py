2875f91a77680aeb6ec22eb0a8a317d109bb699e
Ravali-goud <167891512+Ravali-goud@users.noreply.github.com>
2025-01-02 19:42:15 +0530

    commit3

--- .venv/Home.py
+++ .venv/Home.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 col1,col2=st.columns(2)
 
 with col1:
-    image_path = os.path.join(os.getcwd(), "pythonProject/images/photo.jpg")
+    image_path = os.path.join(os.getcwd(), "pythonProject/images/IMG_20241007_133500.jpg")
 
     if os.path.exists(image_path):
         image = Image.open(image_path)
